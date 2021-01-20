dadf7ad0a3f07448c19eee457f6b63c36983e662
Julian Toumey <jtoumey@gmail.com>
2021-01-20 10:09:04 -0500

    Updated the rcParams template and created a demonstrative plotting script.

---
+++ postProcess/plotting/mplTemplate.py
@@ -0,0 +1,41 @@
+import matplotlib
+import matplotlib.pyplot as plt
+# Read the rcParams file in the same figure
+matplotlib.rc_file('publicationQualityFigures', use_default_template=True)
+
+import numpy as np
+
+# Main plotting routine
+def main():
+
+    # Import the text data
+    solidHtInfo = np.genfromtxt("./wallHeatFluxFlame.dat", skip_header=2)
+
+    # Generate a figure and axes with subplot
+    fig0 = plt.figure(figsize=(5, 7))
+    ax0 = fig0.add_subplot(2, 1, 1)
+
+    # Plot solid data
+    ax0.plot(solidHtInfo[:, 0], -solidHtInfo[:, 4], label='$-$Solid')
+    ax0.set_ylabel('Integrated Heat Flux [W]')
+
+    # Populate additional subplot
+    ax1 = fig0.add_subplot(2, 1, 2)
+    ax1.plot(solidHtInfo[:, 0], -solidHtInfo[:, 2], '-.', marker="^", markerfacecolor='None', markevery=16, label='$-$Solid')
+    ax1.set_xlabel('Time [sec]')
+    ax1.set_ylabel('Heat Flux [W/m\\textsuperscript{2}]')
+
+    ax0.minorticks_on()
+    ax0.grid()
+    ax1.minorticks_on()
+    ax1.grid()
+    ax0.legend(loc='upper right')
+    fig0.tight_layout()
+
+    # Display and then save the figure 
+    plt.show()
+    fig0.savefig(r'sampleFig.pdf', format='pdf', dpi=900)
+
+
+if __name__ == '__main__':
+    main()
